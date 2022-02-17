dfb160993d462669ad242dc340a093dee22c2be7
nazmul huda <nazmul4836@gmail.com>
2022-02-17 19:41:32 +0600

    final commit

---
+++ js/index.js
@@ -0,0 +1,43 @@
+const errorDiv = document.getElementById('error-message');
+const searchResult = document.getElementById('search-result');
+const loadSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // clear previous data
+    searchField.value = '';
+    errorDiv.innerText = '';
+    searchResult.textContent = '';
+    if (searchText === '') {
+        errorDiv.innerText = 'Search field cannot be empty.....!';
+        return;
+    }
+
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearch(data.docs));
+
+};
+
+const displaySearch = (books) => {
+
+    books.forEach(book => {
+        const divBox = document.createElement('div');
+        divBox.classList.add('col');
+        divBox.innerHTML = `
+            <div class="card">
+                  <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top h-100" alt="...">
+                    <div class="card-body">
+                        <h3 class="text-warning">${book.subject_facet}</h3>
+                        <h4 class="text-primary">Author name:${book.author_name}</h4>
+                        <h5 class="text-success">Publisher:${book.publisher}</h5>
+                        <p class="text-info">published date: ${book.first_publish_year}</p>
+                    </div>
+                </div>
+            `;
+        searchResult.appendChild(divBox);
+    });
+
+}
+
+
